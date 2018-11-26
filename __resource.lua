256bc682eed1bd6f48813a83b32dfacb2c977947
zarx1337 <37050859+zarx1337@users.noreply.github.com>
2018-11-26 03:29:29 +0100

    Add files via upload
    Added swedish locale.

--- __resource.lua
+++ __resource.lua
@@ -2,7 +2,21 @@
 --## MADE BY "Bazookan" AND "Viking" PLEASE DO NOT TAKE ANY CREDIT OF THIS!##--
 --###########################################################################--
 -------------------------------------------------------------------------------
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'ESX Signal'
+
+version '1.0.0'
+
+server_scripts {
+    '@es_extended/locale.lua',
+	'locales/sv.lua',
+	'config.lua'
+}
 
 client_scripts {
-    'client.lua'
+    '@es_extended/locale.lua',
+	'locales/sv.lua',
+	'config.lua',
+	'client/main.lua'
 }
